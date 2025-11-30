d8194553a77e80dad143334f2309d9f17c473c07
mou5t4 <att1a@outlook.com>
2025-11-30 19:55:17 +0100

    Fix import in display_manager.py

--- yoyopy/ui/display/display_manager.py
+++ yoyopy/ui/display/display_manager.py
@@ -15,7 +15,7 @@ Date: 2025-11-30
 """
 
 from yoyopy.ui.display.display_factory import get_display
-from yoyopy.ui.display_hal import DisplayHAL
+from yoyopy.ui.display.display_hal import DisplayHAL
 from typing import Optional, Tuple
 from pathlib import Path
 
